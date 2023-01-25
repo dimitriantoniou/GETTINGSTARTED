6dff9e6b424d43d14f93d29f59a430e836bc5bae
Dimitri <dma2012@gmail.com>
2023-01-24 20:38:06 -0600

    added prompt

--- js/external.js
+++ js/external.js
@@ -1,2 +1,5 @@
 console.log("Hello from external JS");
 alert("Welcome to my website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log("The user's favorite color is: " + userInput);
