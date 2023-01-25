4c02ca69226655dfe08aefee4d36f10e9861c590
Dimitri <dma2012@gmail.com>
2023-01-24 20:39:11 -0600

    used prompt with user input

--- js/external.js
+++ js/external.js
@@ -3,3 +3,4 @@ alert("Welcome to my website!");
 
 var userInput = prompt('What is your favorite color?');
 console.log("The user's favorite color is: " + userInput);
+alert("Your favorite color is "+userInput+"!?");
